36b1b9fb952a7ca9401a894ee0f7286c62fe25ce
sagar vashist <sagarheromail10@gmail.com>
2026-07-09 13:24:06 +0530

    chore: remove unused test scripts

--- test-space.js
+++
@@ -1,6 +0,0 @@
-async function test() { 
-  const res = await fetch("http://localhost:3000/api/tmdb?endpoint=search&query=India's%20Got%20Latent%20"); 
-  const data = await res.json();
-  console.log("Trailing space count:", data.data ? data.data.length : data); 
-} 
-test();
